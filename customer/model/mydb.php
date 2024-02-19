8fe4512b9d4142b6483a0922ca8f7f4550954e77
NabilRaiyan <rraiyan77@gmail.com>
2024-02-19 22:33:33 +0600

    Creating mydb file for handling db related code

--- customer/model/mydb.php
+++ customer/model/mydb.php
@@ -7,6 +7,7 @@
             return $conn;
         }
 
+        //adding customer
         function addCustomer($conn, $table, $name, $email, $password, $confirmPassword){
             $sql="INSERT INTO $table (name,email, password, confirmPassword) 
             VALUES ('$name', '$email', '$password', '$confirmPassword')";
