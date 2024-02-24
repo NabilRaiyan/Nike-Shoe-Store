faa40ab01f28a19e4c38e362f2e18a3b36b562bb
NabilRaiyan <rraiyan77@gmail.com>
2024-02-24 21:20:17 +0600

    Creating sign In page

--- customer/views/signIn.php
+++ customer/views/signIn.php
@@ -1,7 +1,5 @@
 <?php 
     include "../controller/process.php";
-    // Header
-    include "../../layouts/header.php";
 ?>
 
 
@@ -10,31 +8,22 @@
 <head>
     <meta charset="UTF-8">
     <meta Name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registration</title>
 </head>
 <body>
 
     <div>
         
     </div>
-    <div class="sign-up-div">
+    <div class="sign-in-div">
         <form action="" method="POST">
-            <h1>Don't have any account? Sign up for free!</h1>
-            <input name="Name" type="text" placeholder="Enter UserName"/>
-            <?php echo $NameError?>
+            <h1>Already have an account? Sign In!</h1>
             <input  name="email" type="text" placeholder="Enter Email"/>
             <?php echo $emailError?>
 
             <input name="password" type="text" placeholder="Enter Password"/>
             <?php echo $passwordError?>
 
-            <input name="confirmPassword" type="text" placeholder="Re-Enter Password"/>
-            <?php echo $confirmPasswordError?>
-
-
-
-
-            <button class="signupButton" type="submit" name="Submit">SIGN UP</button>
+            <button class="signInButton" type="submit" name="Submit">SIGN UP</button>
         </form>
 
         <!-- Footer  -->
